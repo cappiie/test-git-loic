5a8ffe5eb746eb890a7f6c311b20ba150abcfbdc
cappiie <loicflament62@gmail.com>
2018-12-06 08:00:42 +0100

    Index.php ajouté

---
+++ index.php
@@ -0,0 +1,12 @@
+<html class="no-js" lang="fr" prefix="og: http://ogp.me/ns#">
+<head>
+	<link href="styles/stylesheets/screen.css" rel="stylesheet">
+<head>
+<body>
+	Hello World
+	<form method="post" action="">
+		<input type="text" name="nom" />
+		<input type="submit" name="OK" />
+	</form>
+</body>
+</html>
